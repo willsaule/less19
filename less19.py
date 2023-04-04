ceaeda3bd2dcc34583655e08a334f1fd8546cb81
willsaule <avasjashasynov@gmail.com>
2023-04-04 10:10:05 +0600

    first commit

---
+++ less19.py
@@ -0,0 +1,38 @@
+
+
+# dictionary_1 = {'a':300,'b':400} 
+# dictionary_2 = {'c':500,'d':600}
+# dict_3 = {}
+
+# for i in (dictionary_1,dictionary_2):
+#     dict_3.update(i)
+# print(dict_3)
+    
+# dictionary_1 = {'a':300,'b':400}
+# dictionary_2 = {'c':500,'d':600}
+# dict_3 = {}
+
+# for i in zip(dictionary_1.items(),dictionary_2.items()):
+#      dict_3.update(i)
+# print(dict_3)
+
+my_dictionary = {'data1':5,'data2':6,'data3':7,'data4':8}
+    result = 1
+
+for i in my_dictionary:
+    result *= 
+
+
+
+# Создайте словарь, в котором ключами будут числа от 1 до 10, а значениями эти же числа, возведенные в куб.
+
+
+# Циклом for сделать фильтрацию  вывести только тип данных цифр 
+
+# Lst = [1,2,’aasf','1','123',123]
+
+# Вывод:
+# [1,2,123
+
+
+
